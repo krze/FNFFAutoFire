0cb77d823a93f11ac9bea736698cd8bb8426e7da
krze <kenkrzeminski@gmail.com>
2018-11-18 15:16:27 -0500

    Prettier logs

--- README.md
+++ README.md
@@ -1,17 +1,48 @@
 # FNFFAutoFire
-This is a dead-simple command line calculator for Cyberpunk 2020's FNFF automatic
-fire rules.
+This is a dead-simple command line calculator for Cyberpunk 2020's FNFF automatic fire rules. This automatic fire calculator does not permit you to aim at a specific location.
+
+You can use this calculator with single shots or 3-round-burst as long as you are not aiming for a specific location.
 
 ## How to use
 Run with `ruby fnff_auto.rb` and answer the questions in the prompt.
 
+At the end of the prompts, you'll get a result that looks like this:
+
+```
+Calculating damage of 13 against target's armor.
+13 damage penetrated armor!
+Target's BTM 0 has reduced the damage to 13
+Applying 26 damage to HEAD
+
+Results:
+
+LEFT ARM took 0 damage.
+RIGHT ARM took 0 damage.
+LEFT LEG took 0 damage.
+RIGHT LEG took 0 damage.
+TORSO took 0 damage.
+HEAD took 26 damage.
+HEAD was DESTROYED!!
+
+Cover remaining:
+SPS armor is now 10
+
+Victim's current damage is: 26
+VICTIM IS DEFINITELY DEAD!!!
+
+Victim is now DEAD 0. Every minute increases DEAD state +2.
+If the victim reaches DEAD 10 without medical attention, sell em for parts!
+```
+
 ## Armor rules
-This currently uses the 'old' armor values (no bonus for stacking armor), and
-takes cover SPS values into account
+This calculator follows the 'New Armor Rules' from Cyberpunk 2020 2nd Edition. Layered armor will add a bonus depending on the difference between the outer layer and the inner layer.
+
+Armor that is penetrated will have one SPS point deducted. All subsequent shots will be checked against the most up-to-date value.
 
 ## Cover rules
-Partial cover will redirect all shots to the torso and head. Full cover assumes
-you're firing through a wall at the target and will hit all parts of the body.
+If you indicate the target is behind cover, the cover will protect the target for shots to all locations. The cover's SPS value is added to the outer layer of armor for all shots.
+
+'Partial Cover' considers the target to be covered from the waist down. Shots that hit the legs will take into account the cover's SPS value.
 
 ## Fragile!
 I made this in an hour. It expects integers and y/n values only. Be gentle.
--- fnff_auto.rb
+++ fnff_auto.rb
@@ -1,5 +1,3 @@
-require 'pp'
-
 def roll(number, sides)
     total = 0
 
@@ -27,6 +25,25 @@ def location(roll_value)
   end
 end
 
+def stun_or_mortal_check(damage)
+  return nil if damage == 0
+
+  mortal = "MORTAL"
+  mortal_value = 0
+  stun = "STUN"
+  stun_value = damage >= 40 ? 9 : (damage / 4) - (damage % 4 == 0 ? 1 : 0)
+  result = ""
+
+  if damage > 12
+    mortal_value = damage >= 40 ? 6 : ((damage / 4) - 3) - (damage % 4 == 0 ? 1 : 0)
+    result += "#{mortal} -#{mortal_value}\n"
+  end
+
+  result += "#{stun} -#{stun_value}"
+
+  return result
+end
+
 def above_partial_cover(location_value)
   location_value != :right_leg || location_value != :left_leg
 end
@@ -36,11 +53,10 @@ def find_next_layer(armor, current_layer)
 
   # Start the peek at the next layer
   mutable_layer_count = current_layer
-  puts("Seeking the next layer. Checking #{mutable_layer_count}")
+
   # Keep peeking until you run out of layers
   until (armor.key?(mutable_layer_count) || mutable_layer_count <= next_layer)
     mutable_layer_count -= 1
-    puts("Skipping to layer #{mutable_layer_count}")
   end
 
   next_layer = mutable_layer_count
@@ -58,16 +74,18 @@ def reduce_by_armor(hit_damage, hit_location, armor, cover_armor_value, partial_
 
   until (current_layer == 0 || hit_damage <= 0)
     hit_location_armor = behind_cover ? armor[current_layer][:sps] : armor[current_layer][hit_location]
+    current_layer_string = current_layer == :cover ? current_layer.to_s.upcase : "Armor Layer #{current_layer}"
 
-    puts "\nCalculating hit for layer #{current_layer}, in location #{hit_location}"
-    puts "The next layer is #{next_layer}"
-    puts "The current layer's armor is #{hit_location_armor}"
+    puts "\n#{hit_damage} damage strikes #{current_layer == :cover ? "#{current_layer_string}, protecting the #{hit_location.to_s.upcase.sub('_', ' ')}" : "#{current_layer_string} in the #{hit_location.to_s.upcase.sub('_', ' ')}"}"
+    puts "#{current_layer_string}\'s SPS vaue is is #{hit_location_armor}"
+
     if hit_location_armor > 0
       sps_diff_bonus = 0
       next_layer_armor = armor.key?(next_layer) ? armor[next_layer][hit_location] : 0
-      if next_layer >= 1
+
+      if (next_layer >= 1 && next_layer_armor > 0)
         sps_diff = (hit_location_armor - next_layer_armor).abs
-        puts "The next layer armor is #{next_layer_armor}."
+        puts "The next layer armor is #{next_layer_armor}"
         puts "The diff between the two layers is #{sps_diff}."
         if sps_diff < 27
           case sps_diff
@@ -87,28 +105,27 @@ def reduce_by_armor(hit_damage, hit_location, armor, cover_armor_value, partial_
         end
       end
 
-      puts "Hit location armor is #{hit_location_armor}, with sps diff bonus of #{sps_diff_bonus}"
+      puts "#{hit_location.to_s.upcase} is protected with sps diff bonus of #{sps_diff_bonus}" if sps_diff_bonus > 0
       puts "Total hit damage is #{hit_damage}"
       hit_damage = hit_damage - (hit_location_armor + sps_diff_bonus)
       puts "Reduced hit damage is #{hit_damage}"
 
-
       # TODO: AP
       if hit_damage > 0
-        puts("Shot penetrated layer #{current_layer} to hit #{hit_location}")
+        puts("#{hit_damage} damage penetrated layer #{current_layer} to hit #{hit_location.to_s.upcase.sub('_', ' ')}")
         if current_layer == :cover
           armor[current_layer][:sps] = hit_location_armor - 1
         else
           armor[current_layer][hit_location] = hit_location_armor - 1
         end
 
-        puts("Layer #{current_layer} armor in location #{hit_location} is now #{hit_location_armor - 1}")
+        puts("#{current_layer_string}\'s SPS value#{current_layer == :cover ? '' : "in location #{hit_location.to_s.upcase.sub('_', ' ')}"} is now #{hit_location_armor - 1}")
         # Destroy the cover if it's been penetrated
         if (current_layer == :cover && armor[current_layer][:sps] <= 0)
           armor.delete(current_layer)
         end
       else
-        puts("Shot failed to penetrate.\n")
+        puts("Damage failed to penetrate.\n")
         return current_layer == :cover ? nil : hit_damage
       end
     end
@@ -116,9 +133,9 @@ def reduce_by_armor(hit_damage, hit_location, armor, cover_armor_value, partial_
     current_layer = next_layer
     next_layer = find_next_layer(armor, next_layer - 1)
     behind_cover = false
-    puts("Continuing damage calculation for next layer...")
+    puts("Continuing damage calculation for next layer...") if current_layer > 0
   end
-
+  puts("#{hit_damage} damage penetrated #{current_layer == :cover ? 'cover' : 'armor'} to hit the target!")
   return hit_damage
 end
 
@@ -175,6 +192,7 @@ puts "Armor provided by environmental cover is calculated already. Do not enter 
 
 all_armor = Hash.new
 armor_done = false
+target_unarmored = false
 
 no_armor_left = {
   :head => false,
@@ -211,6 +229,11 @@ until armor_done
   # Armor is done if there's no location that includes "false"
   armor_done = !no_armor_left.values.include?(false)
 
+  if (armor_done && current_layer == 1)
+    all_armor[:layer_count] = 0
+    (target_unarmored = true) unless behind_cover
+  end
+
   unless armor_done
     all_armor[current_layer] = armor
     all_armor[:layer_count] = current_layer
@@ -228,24 +251,36 @@ if behind_cover
   end
 end
 
-until (dead || number_of_hits <= 0)
+hit_number = 0
+
+until number_of_hits <= 0
   hit_damage = roll(damage_dice_number, damage_dice_sides) + damage_modifier
   hit_location = location(roll(1,10))
-
-  hit_damage = reduce_by_armor(hit_damage, hit_location, all_armor, cover_armor_value, partial_cover)
+  hit_number += 1
+  puts "\n#{hit_number == 1 ? 'First' : 'Next'} hit..."
+  unless target_unarmored
+    puts "\nApplying damage of #{hit_damage} against target's armor:"
+    hit_damage = reduce_by_armor(hit_damage, hit_location, all_armor, cover_armor_value, partial_cover)
+  else
+    puts "\nTarget is not under cover or wearing armor. #{hit_damage} damage hits!"
+  end
 
   # Hit damage came back nil because it failed to penetrate cover
   next unless hit_damage
 
   hit_damage = hit_damage - btm
 
+  puts "Target's BTM #{btm} has reduced the damage to #{hit_damage}"
+
   # Always apply at least 1 damage
   if hit_damage <= 0
     hit_damage = 1
+    puts "Target still needs to take 1 damage."
   end
 
   # Apply hit damage to location
   hit_damage = hit_damage * 2 if hit_location == :head
+  puts "Applying #{hit_damage} damage to #{hit_location.to_s.upcase.sub('_', ' ')}"
   location_damage[hit_location] = location_damage[hit_location] + hit_damage
   unless hit_location == :torso || hit_damage < 8
     location_damage[:destroyed_limbs] << hit_location
@@ -253,15 +288,14 @@ until (dead || number_of_hits <= 0)
 
   total_damage = total_damage + hit_damage
 
-  dead = true if (total_damage > 40 || location_damage[:head] > 8)
   number_of_hits = number_of_hits - 1
 end
 
 puts "\nResults:\n\n"
-puts "VICTIM IS DEFINITELY DEAD!!\nHere's the obituary:\n\n" if dead
-puts "Victim is now at #{total_damage} damage"
 
+dead = (total_damage > 40)
 limb_destroyed = false
+head_destroyed = false
 
 location_damage.each do |part, amount|
   next if part == :destroyed_limbs
@@ -270,14 +304,18 @@ location_damage.each do |part, amount|
   puts "#{part_string} took #{amount} damage."
 
   next if part == :torso
-  puts "#{part_string} was DESTROYED!!" if location_damage[:destroyed_limbs].include?(part)
-end
+  if location_damage[:destroyed_limbs].include?(part)
+    puts "#{part_string} was DESTROYED!!"
 
-pp all_armor
+    head_destroyed = true if part == :head
+    next if part == :head
+    limb_destroyed = true
+  end
+end
 
 all_armor.each do |layer, armor|
   next if layer == :layer_count
-  puts "Layer #{layer}:"
+  layer == :cover ? puts("\nCover remaining:") : puts("\nLayer #{layer} remaining:")
   armor.each do |part, amount|
     part_string = part.to_s.sub('_', ' ').upcase
 
@@ -285,5 +323,19 @@ all_armor.each do |layer, armor|
   end
 end
 
-puts "A mortal save is necessary due to the loss of one or more limbs." if (limb_destroyed && !dead)
+puts "\nVictim's current damage is: #{total_damage}"
+puts "VICTIM IS DEFINITELY DEAD!!!" if dead || head_destroyed
 puts "The cover was DESTROYED!" if (behind_cover && all_armor[:cover][:sps] <= 0)
+
+mortal_check_result = stun_or_mortal_check(total_damage)
+
+if (!dead && mortal_check_result)
+  puts "\nYour damage of #{total_damage} indicates you must check the following rolls, in order:\n\n"
+  puts "MORTAL -0 for loss of limb(s)" if limb_destroyed
+  puts "#{mortal_check_result}"
+  puts "\nFor each roll, subtract the penalty listed from a D10 roll."
+  puts "The result must be EQUAL OR LESS THAN your stun save save value."
+end
+
+puts "\nVictim is now DEAD 0. Every minute increases DEAD state +2." if dead
+puts "If the victim reaches DEAD 10 without medical attention, sell em for parts!" if dead
